92c59f925ca62086a87dd055e1713d70b38b899a
ppcrong <ppcrong@gmail.com>
2017-06-06 17:10:48 +0800

    [ppcrong.2017.06.06][blehelper] Rename package name

--- src/androidTest/java/com/ppcrong/blehelper/ExampleInstrumentedTest.java
+++ src/androidTest/java/com/ppcrong/blehelper/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.ppcrong.blescanner.test", appContext.getPackageName());
+        assertEquals("com.ppcrong.blehelper.test", appContext.getPackageName());
     }
 }
